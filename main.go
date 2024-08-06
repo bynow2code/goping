ba91c251ce5d3e3d65ac3f6f3b199b84bc95b02d
changqianqian <bynow2code@qq.com>
2024-08-07 01:36:15 +0800

    feat:icmp

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net"
+	"time"
+)
 
+type ICMP struct {
+	Type           uint8
+	Code           uint8
+	Checksum       uint16
+	Identifier     uint16
+	SequenceNumber uint16
+}
+
+func init() {
+	log.SetFlags(log.Llongfile)
+}
+
+func main() {
+	timeout := 3000 * time.Millisecond
+	conn, err := net.DialTimeout("ip:icmp", "bing.com", timeout)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+	defer conn.Close()
+
+	conn.SetDeadline(time.Now().Add(timeout))
 }
