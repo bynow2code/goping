b83467240721165a30e0bb649aefa1828a0b5db4
changqianqian <bynow2code@qq.com>
2024-08-07 02:00:11 +0800

    feat:icmp

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"log"
 	"net"
 	"time"
@@ -28,4 +31,61 @@ func main() {
 	defer conn.Close()
 
 	conn.SetDeadline(time.Now().Add(timeout))
+
+	icmp := ICMP{
+		Type:           8,
+		Code:           0,
+		Checksum:       0,
+		Identifier:     1,
+		SequenceNumber: 1,
+	}
+
+	var buffer bytes.Buffer
+
+	binary.Write(&buffer, binary.BigEndian, icmp)
+	data := make([]byte, 6)
+	binary.Write(&buffer, binary.BigEndian, data)
+
+	request := buffer.Bytes()
+	checkSum := calculateICMPChecksum(request)
+	request[2] = byte(checkSum >> 8)
+	request[3] = byte(checkSum)
+
+	fmt.Println(request)
+
+	_, err = conn.Write(request)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	response := make([]byte, 1024)
+	_, err = conn.Read(response)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+}
+
+func calculateICMPChecksum(buf []byte) uint16 {
+	var sum uint32
+	// 处理奇数长度的情况
+	if len(buf)%2 != 0 {
+		buf = append(buf, 0)
+	}
+
+	// 对所有16位字做二进制加法
+	for i := 0; i < len(buf); i += 2 {
+		word := binary.BigEndian.Uint16(buf[i : i+2])
+		sum += uint32(word)
+	}
+
+	// 加上进位
+	for sum>>16 > 0 {
+		sum = (sum & 0xFFFF) + (sum >> 16)
+	}
+
+	// 取反得到校验和
+	checksum := ^uint16(sum)
+	return checksum
 }
